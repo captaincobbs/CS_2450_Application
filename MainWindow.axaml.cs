2bfc4c4a414b263a6a83746e512a0bb47e8e4a82
Captain Cobbs <egamezhq@gmail.com>
2024-09-10 15:02:47 -0600

    Added example code

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Tmds.DBus.Protocol;
 
 namespace CS_2450_Application;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void On_ButtonClick(object sender, RoutedEventArgs args)
+    {
+        message.Text = "Button clicked!";
     }
 }
