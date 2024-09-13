de29b163fe5ffabd6e5b1352c97bca2a8d3e6c6e
AutonomyCOG <egamezhq@gmail.com>
2024-09-12 22:29:50 -0600

    Rearranged project to include automated tests

---
+++ AutomatedTests/Tests.cs
@@ -0,0 +1,83 @@
+using UVSim;
+
+namespace AutomatedTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+        }
+
+        [TestMethod]
+        public void Test7()
+        {
+        }
+
+        [TestMethod]
+        public void Test8()
+        {
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+        }
+
+        [TestMethod]
+        public void Test11()
+        {
+        }
+
+        [TestMethod]
+        public void Test12()
+        {
+        }
+
+        [TestMethod]
+        public void Test13()
+        {
+        }
+
+        [TestMethod]
+        public void Test14()
+        {
+        }
+
+        [TestMethod]
+        public void Test15()
+        {
+        }
+    }
+}
