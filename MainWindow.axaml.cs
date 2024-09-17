858313e905337d831d540d16b7e4878312208f28
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2024-09-17 16:20:31 -0600

    Add files via upload
    Forgot to add this with the MainWindow.axaml file

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,17 +1,48 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Tmds.DBus.Protocol;
+using System.Collections.Generic;
 
-namespace CS_2450_Application;
+
+namespace AvaloniaApplication1;
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        // DataContext = this;
+        // public MyItem item = new MyItem();
+        // item.Text1 = "01"
+        
+        var items = new List<MyItem>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            items.Add(new MyItem
+            {
+                Text1 = "Hello World",
+                InputText = $"Input {i}",
+                Text2 = "Stuff"
+            });
+        }
+
+        // Assign items to the ListBox using ItemsSource
+        MyListBox.ItemsSource = items;
     }
 
-    public void On_ButtonClick(object sender, RoutedEventArgs args)
+// public string Test => "This is a test";
+    
+    public class MyItem
     {
-        message.Text = "Button clicked!";
+        public string? Text1 { get; set; }
+        public string? InputText { get; set; }
+        public string? Text2 { get; set; }
+    }
+
+    public void RunProg(object sender, RoutedEventArgs args)
+    {
+        Message.Text = "Ran";
     }
 }
+
