3299cca7e2d9113a035d0fab66d76c6da0289329
Captain Cobbs <egamezhq@gmail.com>
2024-10-03 16:31:58 -0600

    Added file select dialog

--- Application/MainWindow.axaml.cs
+++ Application/MainWindow.axaml.cs
@@ -1,12 +1,18 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using UVSim;
 
 
 namespace CS_2450_Application;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
@@ -26,6 +32,18 @@ public partial class MainWindow : Window
         // //
         // Assign items to the ListBox using ItemsSource
         // MyListBox.ItemsSource = items;
+
+        for (int i = 0; i < 5; i++)
+        {
+            var textBlock = new TextBlock
+            {
+                Text = $"Text Block {i + 1}",
+                FontSize = 16,
+                Margin = new Thickness(5)
+            };
+
+            StackPanelCodeSpace.Children.Add(textBlock);
+        }
     }
 
     public static string Test => "This is a test";
@@ -37,9 +55,16 @@ public partial class MainWindow : Window
     //     public string? Text2 { get; set; }
     // }
 
-    public void RunProg(object sender, RoutedEventArgs args)
+    public async void OnUpload_Click(object sender, RoutedEventArgs args)
     {
-        //Message.Text = "Ran";
+        var dialog = new OpenFileDialog
+        {
+            Title = "Select a File",
+            AllowMultiple = false // Set to true if you want to allow multiple file selection
+        };
+
+        // Show the dialog and wait for the result
+        var result = await dialog.ShowAsync(this);
     }
 }
 
