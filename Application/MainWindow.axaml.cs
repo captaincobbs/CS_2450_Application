938026b54a117d7a2c5560095aa153b64c0d7741
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2024-10-03 16:34:11 -0600

    deleted comments

--- Application/MainWindow.axaml.cs
+++ Application/MainWindow.axaml.cs
@@ -18,21 +18,6 @@ public partial class MainWindow : Window
         InitializeComponent();
         DataContext = this;
 
-        // var items = new List<MyItem>();
-        //
-        // for (int i = 0; i < 100; i++)
-        // {
-        //     items.Add(new MyItem
-        //     {
-        //         Text1 = "Hello World",
-        //         InputText = $"Input {i}",
-        //         Text2 = "Stuff"
-        //     });
-        // }
-        // //
-        // Assign items to the ListBox using ItemsSource
-        // MyListBox.ItemsSource = items;
-
         for (int i = 0; i < 5; i++)
         {
             var textBlock = new TextBlock
@@ -47,13 +32,7 @@ public partial class MainWindow : Window
     }
 
     public static string Test => "This is a test";
-
-    // public class MyItem
-    // {
-    //     public string? Text1 { get; set; }
-    //     public string? InputText { get; set; }
-    //     public string? Text2 { get; set; }
-    // }
+    
 
     public async void OnUpload_Click(object sender, RoutedEventArgs args)
     {
