bfde0ad3e6b29b9436f8365cf5a006549d46a767
Captain Cobbs <egamezhq@gmail.com>
2024-10-03 16:15:43 -0600

    Got frontend and backend working at the same time

--- Application/MainWindow.axaml.cs
+++ Application/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System.Collections.Generic;
+
 
 namespace CS_2450_Application;
 
@@ -8,10 +10,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+
+        // var items = new List<MyItem>();
+        //
+        // for (int i = 0; i < 100; i++)
+        // {
+        //     items.Add(new MyItem
+        //     {
+        //         Text1 = "Hello World",
+        //         InputText = $"Input {i}",
+        //         Text2 = "Stuff"
+        //     });
+        // }
+        // //
+        // Assign items to the ListBox using ItemsSource
+        // MyListBox.ItemsSource = items;
     }
 
-    public void On_ButtonClick(object sender, RoutedEventArgs args)
+    public static string Test => "This is a test";
+
+    // public class MyItem
+    // {
+    //     public string? Text1 { get; set; }
+    //     public string? InputText { get; set; }
+    //     public string? Text2 { get; set; }
+    // }
+
+    public void RunProg(object sender, RoutedEventArgs args)
     {
-        message.Text = "Button clicked!";
+        //Message.Text = "Ran";
     }
 }
+
--- Application/Program.cs
+++ Application/Program.cs
@@ -6,26 +6,24 @@ namespace CS_2450_Application;
 
 class Program
 {
-    // Lines 9 - 22 for GUI version
-    //
-    //// Initialization code. Don't use any Avalonia, third-party APIs or any
-    //// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    //// yet and stuff might break.
-    //[STAThread]
-    //public static void Main(string[] args) => BuildAvaloniaApp()
-    //    .StartWithClassicDesktopLifetime(args);
+    //Lines 9 - 22 for GUI version
 
-    //// Avalonia configuration, don't remove; also used by visual designer.
-    //public static AppBuilder BuildAvaloniaApp()
-    //    => AppBuilder.Configure<App>()
-    //        .UsePlatformDetect()
-    //        .WithInterFont()
-    //        .LogToTrace();
-
-    // Main function for console application
+    //Initialization code. Don't use any Avalonia, third-party APIs or any
+    //SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    //yet and stuff might break.
+    [STAThread]
     public static void Main(string[] args)
     {
         UVSim.OperatingSystem virtualMachine = new UVSim.OperatingSystem();
-        virtualMachine.BootToTextUI();
+
+        BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
     }
+
+    //// Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
