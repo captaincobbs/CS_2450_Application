6f072378251cb08caf93295b86914e60a5e778f8
AutonomyCOG <egamezhq@gmail.com>
2024-10-03 22:19:37 -0600

    Attempted data binding

--- Application/MainWindow.axaml.cs
+++ Application/MainWindow.axaml.cs
@@ -1,10 +1,8 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.IO;
+using System.Collections.ObjectModel;
 using UVSim;
 
 
@@ -12,38 +10,94 @@ namespace CS_2450_Application;
 
 public partial class MainWindow : Window
 {
-
+    public ViewModel viewModel;
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = this;
-
-        for (int i = 0; i < 5; i++)
-        {
-            var textBlock = new TextBlock
-            {
-                Text = $"Text Block {i + 1}",
-                FontSize = 16,
-                Margin = new Thickness(5)
-            };
-
-            StackPanelCodeSpace.Children.Add(textBlock);
-        }
+        viewModel = new();
+        DataContext = viewModel;
+        viewModel.AddItem(0, "xxxx", BasicML.READ);
+        viewModel.AddItem(1, "xxxx", BasicML.READ);
+        viewModel.AddItem(2, "xxxx", BasicML.READ);
+        viewModel.AddItem(3, "xxxx", BasicML.READ);
+        viewModel.AddItem(4, "xxxx", BasicML.READ);
+        viewModel.AddItem(5, "xxxx", BasicML.READ);
+        viewModel.AddItem(6, "xxxx", BasicML.READ);
+        viewModel.AddItem(7, "xxxx", BasicML.READ);
+        viewModel.AddItem(8, "xxxx", BasicML.READ);
+        viewModel.AddItem(9, "xxxx", BasicML.READ);
     }
 
-    public static string Test => "This is a test";
-    
-
+    #region Events
     public async void OnUpload_Click(object sender, RoutedEventArgs args)
     {
-        var dialog = new OpenFileDialog
+        OpenFileDialog dialog = new()
         {
-            Title = "Select a File",
+            Title = "Load a File",
             AllowMultiple = false // Set to true if you want to allow multiple file selection
         };
 
         // Show the dialog and wait for the result
         var result = await dialog.ShowAsync(this);
     }
+
+    public async void OnSave_Click(object sender, RoutedEventArgs args)
+    {
+        SaveFileDialog dialog = new()
+        {
+            Title = "Save File",
+        };
+
+        // Show the dialog and wait for the result
+        var result = await dialog.ShowAsync(this);
+    }
+    #endregion
+
+    #region Classes
+    public class MemoryLine : ObservableObject
+    {
+        private int _lineNumber;
+        private string? _hexNumber;
+        private BasicML _instruction;
+        public int LineNumber
+        {
+            get { return _lineNumber; }
+            set { SetProperty(ref _lineNumber, value); }
+        }
+
+        public string? HexNumber
+        {
+            get { return _hexNumber; }
+            set { SetProperty(ref _hexNumber, value); }
+        }
+        public BasicML Instruction
+        {
+            get { return _instruction; }
+            set { SetProperty(ref _instruction, value); }
+        }
+    }
+
+    public class ViewModel : ObservableObject
+    {
+        public ObservableCollection<MemoryLine> LoadedMemory { get; set; }
+
+        public ViewModel()
+        {
+            LoadedMemory = new();
+        }
+
+        public void AddItem(int lineNumber, string hexNumber, BasicML instruction)
+        {
+            MemoryLine memory = new()
+            {
+                LineNumber = lineNumber,
+                HexNumber = hexNumber,
+                Instruction = instruction
+            };
+
+            LoadedMemory.Add(memory);
+        }
+    }
+    #endregion
 }
 
